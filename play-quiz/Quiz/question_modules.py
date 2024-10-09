9f6279ce877e2b86d7e54a2335ccaefa9a897eec
Soumyo Jeet <samajdarsoumyajeet0@gmail.com>
2024-10-09 10:27:22 +0530

    Play True or False quiz

---
+++ play-quiz/Quiz/question_modules.py
@@ -0,0 +1,8 @@
+questions = [   
+             {"question":"&quot;HTML&quot; stands for Hypertext Markup Language.","correct_answer":"True"},
+             {"question":"The programming language &quot;Python&quot; is based off a modified version of &quot;JavaScript&quot;.","correct_answer":"False"},
+             {"question":"The Windows 7 operating system has six main editions.","correct_answer":"True"},
+             {"question":"Linus Torvalds created Linux and Git.","correct_answer":"True",},{"question":"The logo for Snapchat is a Bell.","correct_answer":"False"},{"question":"Ada Lovelace is often considered the first computer programmer.","correct_answer":"True"},
+             {"question":"Time on Computers is measured via the EPOX System.","correct_answer":"False"},
+             {"question":"RAM stands for Random Access Memory.","correct_answer":"True",},{"question":"The Windows ME operating system was released in the year 2000.","correct_answer":"True"}
+]
