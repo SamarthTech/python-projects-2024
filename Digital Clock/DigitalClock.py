34b680887f67999a3d622d40e6accd3d32faff5d
ayankhan <165173909+ayankhanjoiya@users.noreply.github.com>
2024-10-12 17:04:54 +0530

    Update DigitalClock.py
    added a light mode to the project along with incresing readability of code.

--- Digital Clock/DigitalClock.py
+++ Digital Clock/DigitalClock.py
@@ -9,12 +9,31 @@ def time():
     label.config(text=string)
     label.after(1000,time)
 
-#setting font-sizeand style
-label=tk.Label(root,font=('calibri',50,'bold'),background='black',foreground='red')#set background color of clock as black and display time and date in red
+# Function to toggle between dark mode and light mode
+def toggle_mode(event=None):
+    global is_dark_mode
+    if is_dark_mode:
+        # Switch to light mode
+        label.config(background='white', foreground='black')
+        root.config(bg='white')
+        is_dark_mode = False
+    else:
+        # Switch back to dark mode
+        label.config(background='black', foreground='red')
+        root.config(bg='black')
+        is_dark_mode = True
 
+# Label to display the clock
+label = tk.Label(root, font=('calibri', 50, 'bold'), background='black', foreground='red')
 
+# Pack the label
 label.pack(anchor='center')
 
+# Call the time function to start the clock
 time()
+
+# Bind the "Shift + D" key to toggle between light and dark modes
+root.bind('<Shift-D>', toggle_mode)
+
 
 root.mainloop()
