7bf76601f995fc29200c2547384bddc51eaf0d56
Anupam Choudhary <87457759+Anupam0-0@users.noreply.github.com>
2024-10-15 16:04:01 +0000

    Added Weather App

---
+++ Weather-App/main.py
@@ -0,0 +1,18 @@
+import requests
+
+api_key = 'Enter your API key here'
+# you can get it from openweathermap.org
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")
