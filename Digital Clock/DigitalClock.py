c314938c9f3b1f3010a390fd2cea52948b2723b8
SaikatDash <151205239+SaikatDash@users.noreply.github.com>
2024-10-21 00:50:31 +0530

    DigitalClock.py

--- Digital Clock/DigitalClock.py
+++ Digital Clock/DigitalClock.py
@@ -1,20 +1,23 @@
-import tkinter as tk #to use tkinder module as tk in code
-from time import strftime
+import tkinter as tk
+import time
 
-root=tk.Tk()
-root.title("Digital Clock")
+def time_update():
+    current_time = time.strftime('%H:%M:%S')
+    current_date = time.strftime('%A, %d %B %Y')
+    clock_label.config(text=current_time)
+    date_label.config(text=current_date)
+    clock_label.after(1000, time_update)
 
-def time():
-    string=strftime('%H:%M:%S %p \n %D')#to display time in Hours:Minutes:Seconds format in onme line and display the date in the next line
-    label.config(text=string)
-    label.after(1000,time)
+root = tk.Tk()
+root.title('Digital Clock')
 
-#setting font-sizeand style
-label=tk.Label(root,font=('calibri',50,'bold'),background='black',foreground='red')#set background color of clock as black and display time and date in red
+root.configure(bg='black')
 
+clock_label = tk.Label(root, font=('calibri', 50, 'bold'), bg='black', fg='green')
+clock_label.pack(pady=20)
 
-label.pack(anchor='center')
+date_label = tk.Label(root, font=('calibri', 20), bg='black', fg='green')
+date_label.pack()
 
-time()
-
+time_update()
 root.mainloop()
