0f2428bd6eed1a4e1e28489df0771ffbb328419d
Soumyo Jeet <samajdarsoumyajeet0@gmail.com>
2024-10-25 16:42:42 +0530

    Added  Number Game

---
+++ Number-Game/number.py
@@ -0,0 +1,48 @@
+import random
+
+level_step = {
+    'h': 5,
+    'm': 10,
+    'e': 15
+}
+
+
+
+def play():
+    number= random.randint(1,100)
+    while True :
+        level= input("SELECT LEVEL:\ntype 'h' for HARD\n type 'm' for MIDIUM\ntype 'e' for EASY\n").lower()
+        try:
+            steps_granted= level_step[level]
+            break
+        except KeyError:
+            print("Invalid Input!")
+
+    print(steps_granted)
+    for i in range(steps_granted, 0, -1):
+        print(f"You have {i} attempts")
+        while True:
+            try :
+                user_chose= int(input("Guess A Number: "))
+                break
+            except ValueError:
+                print("Not A Number! You're asked to guess a NUMBER!!!")
+
+        if user_chose == number:
+            return("CONGO!! you won!!!")
+        elif user_chose > number:
+            print("Guess lesser number")
+        elif user_chose < number:
+            print("Guess higher number")
+        
+    return f"You Loose!!!\n The exact number was {number}"
+    
+
+wish= input("WELCOME IN GUESS THE NUMBER GAME !!!\nStart play!\n'y' for continue or 'n' for not\n")
+while wish == 'y':
+    print(play())
+    wish = input("RPLAY??\n'y' for continue or 'n' for not\n")
+
+else:
+    print("Invalid Input! Bye...")
+
